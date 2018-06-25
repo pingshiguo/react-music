28a69307e6f31f5524a9ee19cf4b2e88e63be63b
pingshiguo <pingshiguo@gmail.com>
2018-06-25 23:47:40 +0800

    add search bar

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
-import { Typography } from '@material-ui/core';
+
+import { get } from '../common/js/axios';
+import { ERR_OK } from '../api/config';
 
 class Search extends Component {
   render () {
     return (
-      <Typography>
-        Search
-      </Typography>
+      <div>
+        <div className="search-bar-wrapper">
+          <div className="flex search-bar">
+            <form className="flex__item flex search-bar__form">
+              <i className="icon icon-search" />
+              <input
+                type="search"
+                className="flex__item search-bar__input"
+                placeholder="搜索歌曲、歌单、专辑"
+                required
+              />
+              <i className="icon icon-clear" />
+            </form>
+            <a
+              href="javascript:;"
+              className="search-bar__cancel-btn"
+            >取消</a>
+          </div>
+        </div>
+
+        <div className="search-result">
+          <h2 className="search-result__title">热门搜索</h2>
+          <div className="search-result__tags">
+            <span className="tag tag_active">{}</span>
+            <span className="tag">{}</span>
+          </div>
+        </div>
+      </div>
     );
   }
 }
